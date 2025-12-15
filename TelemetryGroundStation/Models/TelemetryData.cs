86ca719a360098ed8d61237971a16b0d4c55fff1
Enis Uzun <enisbesiktasli@gmail.com>
2025-12-15 22:42:04 +0300

    Add TelemetryData

---
+++ TelemetryGroundStation/Models/TelemetryData.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TelemetryGroundStation.TelemetryData;
+public class TelemetryData
+{
+    public DateTime TimeStamp { get; set; } //zaman
+    public double Altitude { get; set; } //yükseklik
+     public double Speed { get; set; } //hız
+    public double Temperature { get; set; } // sıcaklık
+
+    public double Longitude { get; set; } //boylam
+    public double Latitude { get; set; } //enlem
+}
